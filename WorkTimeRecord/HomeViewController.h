d14b5516c86b59ad9917f047e30c858ce7237db2
mookyung song <mookyung@HCs-MacBook-Pro.local>
2015-08-19 14:43:35 +0900

    initial commit

---
+++ WorkTimeRecord/HomeViewController.h
@@ -0,0 +1,13 @@
+//
+//  HomeViewController.h
+//  WorkTimeRecord
+//
+//  Created by mookyung song on 8/19/15.
+//  Copyright (c) 2015 moono. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HomeViewController : UIViewController
+
+@end
---
+++ WorkTimeRecord/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  WorkTimeRecord
+//
+//  Created by mookyung song on 8/19/15.
+//  Copyright (c) 2015 moono. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
